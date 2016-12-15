bca6caadb336ae1343294edc67e812f4295048c6
Eric Berquist <eric.berquist@gmail.com>
2016-12-15 12:35:13 -0500

    continue transition from Frank to SMP

---
+++ haswell/qchem/qchem_branches_libresponse.lua
@@ -0,0 +1,26 @@
+whatis("Q-Chem, personal branch for libresponse development")
+
+conflict("qchem")
+
+local apps_root = "/ihome/dlambrecht/erb74/opt/apps/"
+local package_root = pathJoin(apps_root, "qchem/qchem_branches_libresponse")
+
+setenv("QCSCRATCH", pathJoin(os.getenv("HOME"), "scratch/qchem"))
+setenv("QCLOCALSCR", os.getenv("LOCAL"))
+setenv("QC", package_root)
+setenv("QCAUX", pathJoin(package_root, "../qcaux"))
+setenv("QC_EXT_LIBS", pathJoin(package_root, "../qc_ext_libs"))
+setenv("QCREF", pathJoin(package_root, "../qcref"))
+setenv("QCPLATFORM", "LINUX_Ix86_64")
+setenv("QCRSH", "ssh")
+setenv("QCMPI", "openmpi")
+prepend_path("PATH", pathJoin(package_root, "bin"))
+
+local svn_root = "https://jubilee.q-chem.com/svnroot"
+
+setenv("SVNROOT", svn_root)
+setenv("QCSVN", pathJoin(svn_root, "qchem"))
+setenv("QCREFSVN", pathJoin(svn_root, "qchem_dailyref"))
+setenv("QCAUXSVN", pathJoin(svn_root, "qcaux"))
+setenv("QCMANUAL", pathJoin(svn_root, "qchem_manual"))
+setenv("SVN_EDITOR", "emacs")
