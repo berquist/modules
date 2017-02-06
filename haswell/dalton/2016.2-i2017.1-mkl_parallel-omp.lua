399d7a375528ae93db19615602443b0f4ba0bcfe
Eric Berquist <eric.berquist@gmail.com>
2017-02-06 12:15:33 -0500

    new compiler for DALTON on SMP

--- haswell/dalton/2016.2-i2017.1-mkl_parallel-omp.lua
+++ haswell/dalton/2016.2-i2017.1-mkl_parallel-omp.lua
@@ -3,10 +3,9 @@ whatis("DALTON, an overly-complicated high-level quantum chemistry package")
 conflict("dalton")
 
 local apps_root = "/ihome/dlambrecht/erb74/opt/apps/"
-local package_root = pathJoin(apps_root, "dalton/2016.2-i2015.3-mkl_parallel-omp")
+local package_root = pathJoin(apps_root, "dalton/2016.2-i2017.1-mkl_parallel-omp")
 
 setenv("DALTON_ROOT", package_root)
-setenv("WRKDIR", os.getenv("SLURM_SUBMIT_DIR"))
 setenv("DALTON_TMPDIR", os.getenv("LOCAL"))
 
 prepend_path("PATH", pathJoin(package_root, "dalton"))
